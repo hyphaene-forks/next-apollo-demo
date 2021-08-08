9beec1b74194607cc704d9b10713c9f687eda2be
Maximilien Garenne <maximilien.garenne@gmail.com>
2021-08-08 22:55:09 +0200

    feat(test): add a first test

---
+++ server/src/__tests__/nameGenerator.ts
@@ -0,0 +1,14 @@
+import { generateName } from '../utils/nameGenerator';
+
+describe('nameGenerator', () => {
+	it('should generate a string', () => {
+		const name = generateName();
+		expect(typeof name).toBe('string');
+	});
+	it('should generate a lengthy string', () => {
+		const name = generateName();
+		expect(name.length).toBeTruthy();
+	});
+});
+
+generateName();
