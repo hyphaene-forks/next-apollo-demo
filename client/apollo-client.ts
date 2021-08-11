7a79144228a48bc90120d0d57d305342e059ae1a
Maximilien Garenne <maximilien.garenne@gmail.com>
2021-08-11 13:30:50 +0200

    debug: add log

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -1,9 +1,15 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { BACKEND_URL } from "./constants";
-
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { BACKEND_URL } from './constants';
+console.log('BACKEND_URLLLL');
+console.log('BACKEND_URLLLL');
+console.log('BACKEND_URLLLL');
+console.log('BACKEND_URLLLL');
+console.log('BACKEND_URLLLL');
+console.log('BACKEND_URLLLL');
+console.log({ BACKEND_URL });
 const client = new ApolloClient({
-    uri: BACKEND_URL,
-    cache: new InMemoryCache(),
+	uri: BACKEND_URL,
+	cache: new InMemoryCache(),
 });
 
 export default client;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,26 +3,32 @@ import { gql } from '@apollo/client';
 
 import Name from '../components/Name';
 import client from '../apollo-client';
-import { homeHeader } from '../constants';
+import { BACKEND_URL, homeHeader } from '../constants';
 
 export async function getServerSideProps() {
-	const data = await client.query({
-		query: gql`
-			query name {
-				name
-			}
-		`,
-	});
-
-	return {
-		props: { nameData: data },
-	};
+	try {
+		const data = await client.query({
+			query: gql`
+				query name {
+					name
+				}
+			`,
+		});
+		return {
+			props: { nameData: data },
+		};
+	} catch (error) {
+		return {
+			props: { nameData: { loading: true } },
+		};
+	}
 }
 
 export default function Home({ nameData }): JSX.Element {
 	return (
 		<div>
 			<h1>{homeHeader}</h1>
+			<h1>BACKEND_URL: {BACKEND_URL}</h1>
 			Welcome, <Name data={nameData} />
 			<br />
 			<br />
