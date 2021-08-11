6a20c4b1f675177e139d25c90648a58f6bd98542
Maximilien Garenne <maximilien.garenne@gmail.com>
2021-08-11 13:21:51 +0200

    feat(client): configure port and backend url from the outside

--- client/constants.ts
+++ client/constants.ts
@@ -1,3 +1,4 @@
-export const BACKEND_URL = 'http://localhost:5000/graphql';
+export const BACKEND_URL =
+	process.env.BACKEND_URL || 'http://localhost:5000/graphql';
 
 export const homeHeader = 'Welcome to my test';
